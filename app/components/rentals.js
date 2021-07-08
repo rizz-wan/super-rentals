e7a5fa8fb200bd960de4e824763c015e930f80d4
Rizwan Mohammed <v-rizwmo@microsoft.com>
2021-07-08 20:13:20 +0530

    custom styling and clear button

--- app/components/rentals.js
+++ app/components/rentals.js
@@ -1,6 +1,17 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class RentalsComponent extends Component {
   @tracked query = '';
+  @tracked isButtonDisabled = this.query.length <= 0;
+
+  @action clearQuery() {
+    this.query = '';
+    this.updateButtonState();
+  }
+
+  @action updateButtonState() {
+    this.isButtonDisabled = this.query.length <= 0;
+  }
 }
